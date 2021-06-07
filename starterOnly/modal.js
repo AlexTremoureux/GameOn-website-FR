398d2857c592e3d43123b354cf1c690d4725fcbc
AlexTremoureux <alex.tremoureux@gmail.com>
2021-06-07 09:42:01 +0200

    validation des entrées nom/ prénom/ email/ date de naissnce en Javascript

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,18 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const formValid = document.getElementById("submit");
+let formDataAfter = document.getElementsByClassName(".formData");
+let firstName = document.getElementById("first");
+let nameValid = /^[A-Za-z]{2,30}/;
+let lastName = document.getElementById("last");
+let email = document.getElementById("email");
+let emailValid = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i;
+let birthdate = document.getElementById("birthdate");
+let quantity = document.getElementById("quantity");
+let locationEvent = document.getElementsByClassName(".checkbox-input");
+let checkbox1 = document.getElementById("checkbox1");
+let nameForm = [firstName, lastName];
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +32,51 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Validation formulaire
+formValid.addEventListener("click", validation)
 
+function validation(event){
+  for (let element of nameForm){
+    if (element.validity.valueMissing){
+      event.preventDefault();
+      element.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ.");
+    }else if (nameValid.test(element.value) == false){
+      event.preventDefault();
+      element.closest(".formData").setAttribute("data-error", "Veuillez entrer 2 caractères ou plus pour ce champ.");
+    }else{
+      element.closest(".formData").setAttribute("data-error", "Champ correctement rempli.");
+      element.closest(".formData").style.color="green";
+    }
+  }
+  if (email.validity.valueMissing){
+    event.preventDefault();
+    email.closest(".formData").setAttribute("data-error", "Veuillez renseigner ce champ ");
+  }else if (emailValid.test(email.value) == false){
+    event.preventDefault();
+    email.closest(".formData").setAttribute("data-error", "Veuillez une adresse email valide");
+  }else{
+    email.closest(".formData").setAttribute("data-error", "Adresse email valide");
+    email.closest(".formData").style.color="green";
+  }
+  if (birthdate.validity.valueMissing){
+    event.preventDefault();
+    birthdate.closest(".formData").setAttribute("data-error", "Vous devez entrer votre date de naissance.");
+  }else {
+    event.preventDefault();
+    birthdate.closest(".formData").setAttribute("data-error", "Date de naissance valide");
+    birthdate.closest(".formData").style.color="green";
+  }
+  if (quantity.validity.valueMissing){
+    event.preventDefault();
+    quantity.closest(".formData").setAttribute("data-error", "Veuillez renseigner ce champ ");
+  }else {
+    event.preventDefault();
+    quantity.closest(".formData").setAttribute("data-error", "ok");
+    quantity.closest(".formData").style.color="green";
+  }
+  for (let city of locationEvent){
+    if (city.validity.value){
+      
+    }
+  }
+}
