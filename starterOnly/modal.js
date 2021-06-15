731d5b48f5e2a53a6da8e3e2e72a46da2a8e604a
AlexTremoureux <alex.tremoureux@gmail.com>
2021-06-15 15:11:01 +0200

    Ajout de la fonction snackbar à l'envoi du formulaire, séparation en plusieurs fichiers js pour une meilleure lisibilité, commentaires.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,12 @@
+import {modalbg, quantity} from './variable.js';
+import {modalBtn} from './variable.js';
+import {modalClose} from './variable.js';
+import {formValid} from './variable.js';
+import {nameInput} from './function.js';
+import {conditionsOfUse} from './function.js';
+import {countCheckbox} from './function.js';
+import {snackbar} from './function.js';
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,51 +16,7 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const modalClose = document.querySelector(".close");
-const formData = document.querySelectorAll(".formData");
-const formValid = document.getElementById("submit");
-const inputValid = document.querySelectorAll("data-error");
-let formDataAfter = document.getElementsByClassName(".formData");
-let firstName = document.getElementById("first");
-let nameValid = /^[A-Za-z]{2,30}/;
-let lastName = document.getElementById("last");
-let email = document.getElementById("email");
-let emailValid = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i;
-let birthdate = document.getElementById("birthdate");
-let quantity = document.getElementById("quantity");
-let locationCity = document.getElementsByName("location");
-let checkbox1 = document.getElementById("checkbox1");
 
-const elInput = [
-  {
-    scope : firstName,
-    regex : new RegExp("^[A-Za-z]{2,30}"),
-    errorMessage : "Veuillez rentrer votre prénom, deux caractères minimum."
-  },
-  {
-    scope : lastName,
-    regex : new RegExp("^[A-Za-z]{2,30}"),
-    errorMessage : "Veuillez rentrer votre nom de famille, deux caractères minimum."
-  },
-  {
-    scope : email,
-    regex : new RegExp("^[a-zA-Z0-9\-_]+[a-zA-Z0-9\.\-_]*@[a-zA-Z0-9\-_]+\.[a-zA-Z\.\-_]{1,}[a-zA-Z\-_]+"),
-    errorMessage : "Veuillez entrer une adresse email valide."
-  },
-  {
-    scope : birthdate,
-    regex : new RegExp("^[0-9]"),
-    errorMessage : "Age minimum requis: 12ans"
-  },
-  {
-    scope : quantity,
-    regex : new RegExp("^[0-9]"),
-    errorMessage : "Veuillez entrer un nombre."
-  }
-]
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -71,94 +36,24 @@ modalClose.addEventListener("click", closeModal);
 // Validation formulaire
 formValid.addEventListener("click", validation)
 
+
 //Fonction validation
 function validation(event){ 
 
-  //event.preventDefault();
+  event.preventDefault();
 
-
-  // Validation champ Nom / prénom
-  function nameInput() {
-    for (let element of elInput) {
-      event.preventDefault();
-      if (element.scope.validity.valueMissing) {
-        element.scope.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ.");
-      }else if (!element.regex.test (element.scope.value)) {
-        element.scope.closest(".formData").setAttribute("data-error", element.errorMessage);
-      }else {
-        element.scope.closest(".formData").setAttribute("data-error", "");
-        //return true
-      }
-      //listener sur input et rappel de la fonction 
-      element.scope.addEventListener("blur", function(event) {
-        event.preventDefault();
-        if (element.scope.validity.valueMissing) {
-          element.scope.closest(".formData").setAttribute("data-error", "Veuillez remplir ce champ.");
-        }else if (!element.regex.test (element.scope.value)) {
-          element.scope.closest(".formData").setAttribute("data-error", element.errorMessage);
-        }else{
-          element.scope.closest(".formData").setAttribute("data-error", "");
-        }
-      })
-    }
-  }
-
+  // Validation champ Nom / Prénom / Email / Birthdate / Nombre de tournois
   nameInput();
-  
 
   // Validation Conditions d'utilisation
-  function conditionsOfUse() {
-    if (checkbox1.checked==false) {
-      checkbox1.closest(".formData").setAttribute("data-error", "Veuillez accepter nos conditions d'utilisation ");
-    }else{
-        event.preventDefault();
-        checkbox1.closest(".formData").setAttribute("data-error", "");
-        //return true
-    }
-    //listener sur checkbox1 et rappel de la fonction
-    checkbox1.addEventListener("change", function(event) {
-      if (this.checked==false) {
-        checkbox1.closest(".formData").setAttribute("data-error", "Veuillez accepter nos conditions d'utilisation ");
-      }else{
-        event.preventDefault();
-        checkbox1.closest(".formData").setAttribute("data-error", "");
-      }
-    })
-  }
-  
   conditionsOfUse();
-  
 
   // Validation champ Villes
-  function countCheckbox() {
-    let compteur = 0;
-    for (let i=0; i<locationCity.length; i++) {
-      if (locationCity[i].checked == true) {
-        compteur ++;
-      }
-    }
-    console.log(compteur)
-    for (let elCity of locationCity) {
-      if (compteur<1) {
-        elCity.closest(".formData").setAttribute("data-error", "veuillez cocher une ville")
-      }else {
-        elCity.closest(".formData").setAttribute("data-error", "")
-        //return true
-      }
-      //listener sur elCity et fonction
-      elCity.addEventListener("blur", function(event){
-        if (compteur<1) {
-          elCity.closest(".formData").setAttribute("data-error", "veuillez cocher une ville")
-        }else {
-          elCity.closest(".formData").setAttribute("data-error", "")
-        }
-      })
-    }
+  countCheckbox();
+console.log(quantity.value)
+  // Si toutes les fonctions ont renvoyées true, fonction snackbar qui envoi un message de confirmation d'envoi
+  if (nameInput() && conditionsOfUse() && countCheckbox() == true){
+    snackbar()
   }
 
-  countCheckbox();
-  
-  /*if (nameInput && conditionsOfUse && countCheckbox == !true){
-    alert("formulaire envoyé avec succès")
-  }*/
 }
--- starterOnly/variable.js
+++ starterOnly/variable.js
@@ -1,46 +1,50 @@
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const formValid = document.getElementById("submit");
-const inputValid = document.querySelectorAll("data-error");
-let formDataAfter = document.getElementsByClassName(".formData");
-let firstName = document.getElementById("first");
-let nameValid = /^[A-Za-z]{2,30}/;
-let lastName = document.getElementById("last");
-let email = document.getElementById("email");
-let emailValid = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i;
-let birthdate = document.getElementById("birthdate");
-let quantity = document.getElementById("quantity");
-let locationCity = document.getElementsByName("location");
+// DOM Elements
+export const modalbg = document.querySelector(".bground");
+export const modalBtn = document.querySelectorAll(".modal-btn");
+export const modalClose = document.querySelector(".close");
+export const formData = document.querySelectorAll(".formData");
+export const formValid = document.getElementById("submit");
+export const inputValid = document.querySelectorAll("data-error");
+export const formDataAfter = document.getElementsByClassName(".formData");
+export const firstName = document.getElementById("first");
+export const checkbox1 = document.getElementById("checkbox1");
+export const lastName = document.getElementById("last");
+export const email = document.getElementById("email");
+export const birthdate = document.getElementById("birthdate");
+export const quantity = document.getElementById("quantity");
+export const locationCity = document.getElementsByName("location");
 
-let checkbox1 = document.getElementById("checkbox1");
+//RegExp
+export const nameValid = /^[A-Za-z]{2,30}/;
+export const emailValid = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+export const quantityValid = /^[0-9]$|^[1-9][0-9]$|^$/;
+export const birthdateValid = /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/;
 
-const modalClose = document.querySelector(".close");
-
-const elInput = [
+//Array
+export const elInput = [
   {
     scope : firstName,
-    regex : new RegExp("^[A-Za-z]{2,30}"),
+    regex : nameValid,
     errorMessage : "Veuillez rentrer votre prénom, deux caractères minimum."
   },
   {
     scope : lastName,
-    regex : new RegExp("^[A-Za-z]{2,30}"),
+    regex : nameValid,
     errorMessage : "Veuillez rentrer votre nom de famille, deux caractères minimum."
   },
   {
     scope : email,
-    regex : new RegExp("^[a-zA-Z0-9\-_]+[a-zA-Z0-9\.\-_]*@[a-zA-Z0-9\-_]+\.[a-zA-Z\.\-_]{1,}[a-zA-Z\-_]+"),
+    regex : emailValid,
     errorMessage : "Veuillez entrer une adresse email valide."
   },
   {
     scope : birthdate,
-    regex : new RegExp("^[0-9]"),
-    errorMessage : "Age minimum requis: 12ans"
+    regex : birthdateValid,
+    errorMessage : "Veuillez rentrer une date au format DD/MM/YYYY"
   },
   {
     scope : quantity,
-    regex : new RegExp("^[0-9]"),
-    errorMessage : "Veuillez entrer un nombre."
+    regex : quantityValid,
+    errorMessage : "Veuillez entrer un nombre compris entre 0 et 99."
   }
 ]
